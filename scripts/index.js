c5c37c888eeb660a7d692dfde10adcf6b4f1b058
MauricioNLM <Maurcio.Navarro@sdgku.edu>
2024-10-02 16:46:14 -0700

    Assignment 3

--- scripts/index.js
+++ scripts/index.js
@@ -6,20 +6,84 @@ function save(){
     let status = $('#txtStatus').val()
     let budget = $('#txtBudget').val()
 
-    let task = {title:title,description: description, color: colo, date:standarDate, status : status, budget:budget}
+    let task = new Task (title, description, colo, standarDate, status, budget)
 
 
     console.log(task)
+
+    $.ajax({
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/", //the nURL to sent the request to
+        type:'POST',//the type of request (GET,POST,PUT,DELETE,PARTCH)
+        data:JSON.stringify(task),//conver Objet to JSON
+        contentType:'application/json',
+        success:function(response){
+            console.log(response);
+        },
+        error:function(response){
+            console.log(response);
+        }
+    })
+
+    displayTask(task)
+}
+
+
+function displayTask(task){
+    let sintax = `
+    <div class="task-container" style="border-color:${task.color}">
+        <div class="task">
+        <div class="info">
+            <h5>${task.title} </h5>
+            <p>${task.description}</p>
+        </div>
+            <div class="status">
+            ${task.status}
+        </div>
+        <div class="date-budget">
+            <span>${task.date}</span>
+            <span>${task.budget}</span>
+        </div>
+
+        </div>
+        
+    </div>
+    
+    `;
+
+    $('#list').append(sintax)
+}
+
+function LoadTask(){
+    console.log('LoadTaks')
+
+    $.ajax({
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/", //the nURL to sent the request to
+        type:'GET',//the type of request (GET,POST,PUT,DELETE,PARTCH)
+        success:function(response){
+            let data = JSON.parse(response);
+
+            for(let i=0;i<data.length;i++){
+                let task = data[i];
+                if(task.name === 'Mauricio'){
+                    debugger
+                    displayTask(task);
+                }
+            }
+
+            console.log(response);
+        },
+        error:function(response){
+            console.log(response);
+        }
+    })
+}
+
+
+function init(){
+    $('#btnSave').click(save);
+    LoadTask();
 }
 
 
 
-
-
-
-
-
-
-
-
-window.onloadstart = $('#btnSave').click(save)
+window.onloadstart = init()
